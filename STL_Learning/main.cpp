c9bb3a1a0b28f9760daaa29b75d0aebaec036c6a
lilelr <lilelr@163.com>
2016-09-09 13:30:13 +0800

    Initial Commit

---
+++ STL_Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL_Learning
+//
+//  Created by YuXiao on 9/9/16.
+//  Copyright © 2016 YuXiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
